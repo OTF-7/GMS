aef13f34ac1067252d9019ecbcec1ce299b00234
Omar Taha <a.777118407@gmail.com>
2021-07-11 19:40:37 +0300

    add an activity into the aqel folder for adding a citizen data which the rep should review them and confirm

---
+++ app/src/main/java/com/GMS/aqel/activities/AddCitizenActivity.java
@@ -0,0 +1,16 @@
+package com.GMS.aqel.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.GMS.R;
+
+public class AddCitizenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_citizen);
+    }
+}
