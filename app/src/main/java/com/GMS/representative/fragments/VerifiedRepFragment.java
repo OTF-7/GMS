774dafdda561d95568529d324f7cacf3e44d3c0f
Omar Taha <a.777118407@gmail.com>
2021-08-01 20:25:35 +0300

    not impo....

--- app/src/main/java/com/GMS/representative/fragments/VerifiedRepFragment.java
+++ app/src/main/java/com/GMS/representative/fragments/VerifiedRepFragment.java
@@ -1,6 +1,12 @@
 package com.GMS.representative.fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -8,13 +14,6 @@ import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.GMS.R;
 import com.GMS.databinding.FragmentVerifiedRepBinding;
@@ -28,9 +27,10 @@ import java.util.ArrayList;
 public class VerifiedRepFragment extends Fragment {
 
 
-    public static final int FRAGMENT_ID=2;
-    RecyclerViewRepAdapterCitizen adapter ;
-    FragmentVerifiedRepBinding mBinding ;
+    public static final int FRAGMENT_ID = 2;
+    RecyclerViewRepAdapterCitizen adapter;
+    FragmentVerifiedRepBinding mBinding;
+
     public VerifiedRepFragment() {
         // Required empty public constructor
     }
@@ -39,23 +39,23 @@ public class VerifiedRepFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       mBinding  = FragmentVerifiedRepBinding.inflate(inflater , container , false);
+        mBinding = FragmentVerifiedRepBinding.inflate(inflater, container, false);
 
         ArrayList<CitizenItemOfRep> items = new ArrayList<>();
-        items.add(new CitizenItemOfRep("Abdulrahman Khalid" , "45d55d45s55g" , 3  , R.drawable.ic_identity_card_scan));
-        items.add(new CitizenItemOfRep("Abubaker Khalid" , "45d55d45s55g" , 3 ,R.drawable.ic_identity_card_scan ));
-        items.add(new CitizenItemOfRep("Abdulrahman Khalid" , "45d55d45s55g" , 3 , R.drawable.ic_identity_card_scan));
-        items.add(new CitizenItemOfRep("Abdulrahman Khalid" , "45d55d45s55g" ,  3, R.drawable.ic_identity_card_scan));
-        items.add(new CitizenItemOfRep("Abdulrahman Khalid" , "45d55d45s55g" , 3 , R.drawable.ic_identity_card_scan));
-        items.add(new CitizenItemOfRep("Abdulrahman Khalid" , "45d55d45s55g" , 3 , R.drawable.ic_identity_card_scan));
+        items.add(new CitizenItemOfRep("Abdulrahman Khalid", "45d55d45s55g", 3, R.drawable.ic_identity_card_scan));
+        items.add(new CitizenItemOfRep("Abubaker Khalid", "45d55d45s55g", 3, R.drawable.ic_identity_card_scan));
+        items.add(new CitizenItemOfRep("Abdulrahman Khalid", "45d55d45s55g", 3, R.drawable.ic_identity_card_scan));
+        items.add(new CitizenItemOfRep("Abdulrahman Khalid", "45d55d45s55g", 3, R.drawable.ic_identity_card_scan));
+        items.add(new CitizenItemOfRep("Abdulrahman Khalid", "45d55d45s55g", 3, R.drawable.ic_identity_card_scan));
+        items.add(new CitizenItemOfRep("Abdulrahman Khalid", "45d55d45s55g", 3, R.drawable.ic_identity_card_scan));
 
-        adapter = new RecyclerViewRepAdapterCitizen(items  , FRAGMENT_ID);
+        adapter = new RecyclerViewRepAdapterCitizen(items, FRAGMENT_ID);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         mBinding.rvVerifiedFragment.setHasFixedSize(true);
         mBinding.rvVerifiedFragment.setLayoutManager(layoutManager);
         mBinding.rvVerifiedFragment.setAdapter(adapter);
 
-       return mBinding.getRoot() ;
+        return mBinding.getRoot();
     }
 
     @Override
@@ -66,9 +66,9 @@ public class VerifiedRepFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
-        inflater.inflate(R.menu.search_item_menu , menu);
+        inflater.inflate(R.menu.search_item_menu, menu);
         MenuItem searchItem = menu.findItem(R.id.search_ic);
-        SearchView searchView= (SearchView) searchItem.getActionView();
+        SearchView searchView = (SearchView) searchItem.getActionView();
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
